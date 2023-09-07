ebb596eaa743dd22a662e0a3f5b2c16acee750ed
Moniruzzaman <144223325+devmoniruzzaman@users.noreply.github.com>
2023-09-08 03:48:11 +0600

    functions.php

--- README.md
+++
@@ -1,2 +0,0 @@
-# Change-add-to-cart-text-wooCommerce
-How to Change add to cart text on product archives page
---
+++ functions.php
@@ -0,0 +1,4 @@
+add_filter( 'woocommerce_product_add_to_cart_text', 'woocommerce_add_to_cart_button_text_archives' );  
+function woocommerce_add_to_cart_button_text_archives() {
+    return __( 'View', 'woocommerce' );
+}
